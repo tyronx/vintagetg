048fd941e02ed4347da39a0160fbab313e48ae4b
Tyron Madlener <tyronx@gmail.com>
2015-03-06 18:46:51 +0100

    Updated some infos

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # vc-vanilla-terrain-gen
+
+This is a Variant of my other Projekt Vintagecraft which only uses Vanilla Blocks for Terrain Generation. Probably won't continue on this, unless there is huge interest. Pull requests are welcome.
--- src/main/java/at/tyron/vintagecraft/ModInfo.java
+++ src/main/java/at/tyron/vintagecraft/ModInfo.java
@@ -1,16 +1,16 @@
 package at.tyron.vintagecraft;
 
 public class ModInfo {
-	public static final String ModID = "vintagecraft";
-	public static final String ModName = "VintageCraft";
+	public static final String ModID = "vintage-terrain-generator";
+	public static final String ModName = "Vintage TG";
 
-	public static final int VersionMajor = 0;
-	public static final int VersionMinor = 1;
-	public static final int VersionRevision = 9;
+	public static final int VersionMajor = 1;
+	public static final int VersionMinor = 0;
+	public static final int VersionRevision = 0;
 
 	public static final String ModVersion = VersionMajor+"."+VersionMinor+"."+VersionRevision;
 
-	public static final String ModChannel = "TerraFirmaCraft";
+	public static final String ModChannel = "VintageCraft";
 	public static final String SERVER_PROXY_CLASS = "at.tyron.vintagecraft.CommonProxy";
 	public static final String CLIENT_PROXY_CLASS = "at.tyron.vintagecraft.client.ClientProxy";
 
